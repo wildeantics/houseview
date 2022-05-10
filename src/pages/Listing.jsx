8d5a635fd73944cb3dd351283c0ca8657af76ad8
Morgan Wilde <morgan@wilde.mx>
2022-05-11 08:47:35 +1000

    Fixed Swiper

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,16 +1,18 @@
 import { useState, useEffect } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
-// import { Helmet } from 'react-helmet'
+import { Helmet } from 'react-helmet'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
-import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination, Navigation } from 'swiper'
 import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
 import { getDoc, doc } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { db } from '../firebase.config'
+
 import Spinner from '../components/Spinner'
 import shareIcon from '../assets/svg/shareIcon.svg'
-SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 function Listing() {
   const [listing, setListing] = useState(null)
@@ -41,10 +43,15 @@ function Listing() {
 
   return (
     <main>
-      {/* <Helmet>
+      <Helmet>
         <title>{listing.name}</title>
-      </Helmet> */}
-      <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+      </Helmet>
+      <Swiper
+        slidesPerView={1}
+        pagination={{ clickable: true, dynamicBullets: true }}
+        loop={true}
+        modules={[Pagination, Navigation]}
+      >
         {listing.imageUrls.map((url, index) => (
           <SwiperSlide key={index}>
             <div
